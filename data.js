3e12f6b0acb6187631a2db863db6996e448cc19f
Virtual360Tour <egasymovas@gmail.com>
2022-05-08 15:56:39 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,483 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-entrance",
+      "name": "Front Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.05810908456064112,
+        "pitch": -0.20411224706225184,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03542740688218693,
+          "pitch": -0.4218759067084683,
+          "rotation": 0,
+          "target": "1-ash-grove---ground-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ash-grove---ground-floor",
+      "name": "Ash Grove - Ground Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4303893041597302,
+        "pitch": -0.036482217684252305,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.474911459095507,
+          "pitch": -0.20534181131360363,
+          "rotation": 0,
+          "target": "0-front-entrance"
+        },
+        {
+          "yaw": -0.07076550432490869,
+          "pitch": -0.07092281369507702,
+          "rotation": 0.7853981633974483,
+          "target": "2-reception"
+        },
+        {
+          "yaw": 0.9552932316087652,
+          "pitch": -0.3755443779294865,
+          "rotation": 0.7853981633974483,
+          "target": "7-birch-grove---1st-floor"
+        },
+        {
+          "yaw": 2.779631763632211,
+          "pitch": -0.05265816612765839,
+          "rotation": 0,
+          "target": "3-main-lounge"
+        },
+        {
+          "yaw": 1.55311727624448,
+          "pitch": -0.0389943682762528,
+          "rotation": 5.497787143782138,
+          "target": "6-garden"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-reception",
+      "name": "Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.27648264944879486,
+        "pitch": 0.07724909049576922,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6572455279436777,
+          "pitch": 0.04122526752212252,
+          "rotation": 5.497787143782138,
+          "target": "1-ash-grove---ground-floor"
+        },
+        {
+          "yaw": 1.5166924179360306,
+          "pitch": 0.06257693456702995,
+          "rotation": 0,
+          "target": "5-room-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-main-lounge",
+      "name": "Main Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.5455779934344243,
+        "pitch": 0.16279774173733763,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2703958954576162,
+          "pitch": 0.1343997431864441,
+          "rotation": 18.06415775814132,
+          "target": "4-dining-room"
+        },
+        {
+          "yaw": -2.650216713862214,
+          "pitch": 0.10679973682839616,
+          "rotation": 0,
+          "target": "1-ash-grove---ground-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dining-room",
+      "name": "Dining Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.517956622906846,
+        "pitch": 0.20006286738080448,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8258376975115542,
+          "pitch": 0.0016582827323912142,
+          "rotation": 0,
+          "target": "3-main-lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-room-9",
+      "name": "Room 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1010845813742023,
+          "pitch": 0.10206785143263986,
+          "rotation": 11.780972450961727,
+          "target": "2-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-garden",
+      "name": "Garden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7603255051236282,
+        "pitch": 0.06783485327446037,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24855072406460543,
+          "pitch": -0.07973578441877649,
+          "rotation": 0,
+          "target": "1-ash-grove---ground-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-birch-grove---1st-floor",
+      "name": "Birch Grove - 1st Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.709611965788831,
+          "pitch": 0.03423098732267782,
+          "rotation": 3.9269908169872414,
+          "target": "1-ash-grove---ground-floor"
+        },
+        {
+          "yaw": -1.7109277869805446,
+          "pitch": -0.15505423154261955,
+          "rotation": 5.497787143782138,
+          "target": "8-cedar-grove---2nd-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-cedar-grove---2nd-floor",
+      "name": "Cedar Grove - 2nd Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.3575664568930712,
+        "pitch": 0.0275650105264571,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9951078254061265,
+          "pitch": 0.16221959465727664,
+          "rotation": 3.9269908169872414,
+          "target": "7-birch-grove---1st-floor"
+        },
+        {
+          "yaw": -1.998021274593743,
+          "pitch": -0.020556411717475243,
+          "rotation": 5.497787143782138,
+          "target": "9-dove-grove---3rd-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-dove-grove---3rd-floor",
+      "name": "Dove Grove - 3rd Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.7535720598931341,
+        "pitch": 0.06415236289592485,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5435038709539661,
+          "pitch": -0.08637532923101077,
+          "rotation": 0,
+          "target": "10-upstairs-lounge"
+        },
+        {
+          "yaw": -0.8591236140439733,
+          "pitch": -0.061408108019239904,
+          "rotation": 9.42477796076938,
+          "target": "8-cedar-grove---2nd-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-upstairs-lounge",
+      "name": "Upstairs Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.8570020316509463,
+        "pitch": 0.1695739758342114,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.405948727208192,
+          "pitch": 0.22679544463717072,
+          "rotation": 4.71238898038469,
+          "target": "9-dove-grove---3rd-floor"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
